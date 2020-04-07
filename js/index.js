0ce845030b6dd6ebcd13958fa8ada1a9cb5b65e6
Drake Alia <DrakeTesla@macbook-pro.lan>
2020-04-06 20:40:49 -0700

    added in main content and paragraph along with making the links green

--- js/index.js
+++ js/index.js
@@ -42,13 +42,14 @@ let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 logo.src = siteContent["nav"]["img-src"]
 
-let links = document.getElementsByTagName('a');
-links[0].textContent = siteContent["nav"]["nav-item-1"]
-links[1].textContent = siteContent["nav"]["nav-item-2"]
-links[2].textContent = siteContent["nav"]["nav-item-3"]
-links[3].textContent = siteContent["nav"]["nav-item-4"]
-links[4].textContent = siteContent["nav"]["nav-item-5"]
-links[5].textContent = siteContent["nav"]["nav-item-6"]
+/// Nav //////
+let navLinks = document.getElementsByTagName('a');
+navLinks[0].textContent = siteContent["nav"]["nav-item-1"]
+navLinks[1].textContent = siteContent["nav"]["nav-item-2"]
+navLinks[2].textContent = siteContent["nav"]["nav-item-3"]
+navLinks[3].textContent = siteContent["nav"]["nav-item-4"]
+navLinks[4].textContent = siteContent["nav"]["nav-item-5"]
+navLinks[5].textContent = siteContent["nav"]["nav-item-6"]
 
 let navColor = document.querySelectorAll('a')
 navColor.forEach( element => {
@@ -56,18 +57,48 @@ navColor.forEach( element => {
 });
 
 
+// console.log(navLinks)
+
+///// Call to Action ////
 let title = document.querySelector("h1");
 title.textContent = siteContent["cta"]["h1"];
 
-
 let btn = document.getElementsByTagName('button')
 btn[0].textContent = siteContent["cta"]["button"]
-console.log(btn)
+// console.log(btn)
 
 let ctaLogo = document.querySelector("#cta-img");
 ctaLogo.src = siteContent["cta"]["img-src"];
 
+//// Main Content /////
+let contentH4 = document.getElementsByTagName('h4');
 
+contentH4[0].textContent = siteContent["main-content"]["features-h4"];
+contentH4[1].textContent = siteContent["main-content"]["about-h4"];
+contentH4[2].textContent = siteContent["main-content"]["services-h4"];
+contentH4[3].textContent = siteContent["main-content"]["product-h4"];
+contentH4[4].textContent = siteContent["main-content"]["vision-h4"];
+contentH4[5].textContent = siteContent["contact"]["contact-h4"];
+//console.log(contentH4)
+
+
+let middleImg = document.getElementById('middle-img');
+middleImg.src = siteContent["main-content"]["middle-img-src"];
+
+/// Paragraph /////
+let contentParagraph = document.getElementsByTagName("p");
+
+contentParagraph[0].textContent = siteContent["main-content"]["features-content"];
+contentParagraph[1].textContent = siteContent["main-content"]["about-content"];
+contentParagraph[2].textContent = siteContent["main-content"]["services-content"];
+contentParagraph[3].textContent = siteContent["main-content"]["product-content"];
+contentParagraph[4].textContent = siteContent["main-content"]["vision-content"];
+contentParagraph[5].textContent = siteContent["contact"]["address"];
+contentParagraph[6].textContent = siteContent["contact"]["phone"];
+contentParagraph[7].textContent = siteContent["contact"]["email"];
+
+contentParagraph[8].textContent = siteContent["footer"]["copyright"];
+//console.log(contentParagraph)
 
 
 
