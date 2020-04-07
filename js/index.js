d54ada6f3ec56d964ece02a4df0f1496f2df1f91
Drake Alia <DrakeTesla@macbook-pro.lan>
2020-04-07 12:21:37 -0700

    organized siteContent

--- js/index.js
+++ js/index.js
@@ -51,6 +51,7 @@ navLinks[3].textContent = siteContent["nav"]["nav-item-4"]
 navLinks[4].textContent = siteContent["nav"]["nav-item-5"]
 navLinks[5].textContent = siteContent["nav"]["nav-item-6"]
 
+/// Add Color ///
 let navColor = document.querySelectorAll('a')
 navColor.forEach( element => {
   element.style.color = "green"
